bda6e1c383ef031872967b2476bb1e4231ea2105
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-17 19:25:45 +0200

    [autopush] «This fortune is false.»

--- app/http.go
+++ app/http.go
@@ -2,8 +2,13 @@ package haxsh
 
 import (
 	yosrv "yo/srv"
+	. "yo/util"
 )
 
 func init() {
 	yosrv.AppApiUrlPrefix = "_/"
+	yosrv.AppSideStaticRedirectPathFor = func(requestPath string) string {
+		is_home := (requestPath == "") // `requestPath` never has a leading slash
+		return If(is_home, "__static/home.html", "__static/haxsh.html")
+	}
 }
