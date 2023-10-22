aa7db14b4ffeb6b4ae6607d696d3682a58016ce1
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-22 15:24:41 +0200

    [autopush] «sillema sillema nika su»

--- __static/haxsh.ts
+++ __static/haxsh.ts
@@ -8,7 +8,7 @@ function onErr(err: any) { console.error(err) }
 let loginDialog = () => {
     const in_user_name = htm.input({})
     const in_password = htm.input({ 'type': 'password' })
-    return htm.dialog({},
+    return htm.dialog({ 'open': true },
         in_user_name,
         in_password,
         htm.button({
@@ -18,6 +18,8 @@ let loginDialog = () => {
                     alert("ok!")
                 } catch (err) {
                     switch (err as yo.UserSignInErr) {
+                        case '___yo_authLogin_AccountDoesNotExist':
+                        case '___yo_authLogin_EmailInvalid':
                         case '___yo_authLogin_WrongPassword':
                             alert(err);
                             return;
