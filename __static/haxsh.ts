a20aa87d01e92ad429903f5daa82137c434b4774
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-22 14:04:14 +0200

    [autopush] «sillema sillema nika su»

--- __static/haxsh.ts
+++ __static/haxsh.ts
@@ -1,4 +1,24 @@
+import van from '../__yostatic/vanjs/van-1.2.3.debug.js'
+
+const htm = van.tags
+
+let loginDialog = () => {
+    const in_user_name = htm.input({})
+    const in_password = htm.input({ 'type': 'password' })
+    return htm.dialog({},
+        in_user_name,
+        in_password,
+        htm.button({
+            'onclick': () => {
+                return false
+            }
+        }, "Login")
+    )
+}
 
 export function main() {
-    alert("we're on")
+    const login_dialog = loginDialog()
+    van.add(document.body,
+        login_dialog,
+    )
 }
