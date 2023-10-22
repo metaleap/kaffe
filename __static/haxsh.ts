83d47038b4cf5b16b89a8050761a256874d61c87
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-22 15:10:46 +0200

    [autopush] «Klatu barada nikto.»

--- __static/haxsh.ts
+++ __static/haxsh.ts
@@ -1,6 +1,9 @@
 import van from '../__yostatic/vanjs/van-1.2.3.debug.js'
+import * as yo from './yo-sdk.js'
 
 const htm = van.tags
+
+function onErr(err: any) { console.error(err) }
 
 let loginDialog = () => {
     const in_user_name = htm.input({})
@@ -9,7 +12,18 @@ let loginDialog = () => {
         in_user_name,
         in_password,
         htm.button({
-            'onclick': () => {
+            'onclick': async () => {
+                try {
+                    await yo.apiUserSignIn({ EmailAddr: in_user_name.value, PasswordPlain: in_password.value })
+                    alert("ok!")
+                } catch (err) {
+                    switch (err as yo.UserSignInErr) {
+                        case '___yo_authLogin_WrongPassword':
+                            alert(err);
+                            return;
+                    }
+                    onErr(err)
+                }
                 return false
             }
         }, "Login")
