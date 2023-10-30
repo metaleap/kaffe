e88008677dd3b07613b492434bf4fd0c3beae6a1
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-30 14:37:36 +0100

    fortune sez: «Klatu barada nikto.»

--- app/buddy.go
+++ app/buddy.go
@@ -8,11 +8,19 @@ import (
 )
 
 func userBuddies(ctx *Ctx, forUser *User, normalizeLastSeenByMinute bool) (buddiesAlready []*User, buddyRequestsMade []*User, buddyRequestsBy []*User) {
-	buddiesAlready = yodb.FindMany[User](ctx, UserId.In(forUser.Buddies.ToAnys()...).And(
-		q.InArr(forUser.Id, UserBuddies)), 0, nil, UserLastSeen.Desc(), UserDtMod.Desc())
-	buddyRequestsMade = yodb.FindMany[User](ctx, UserId.In(forUser.Buddies.ToAnys()...).And(
-		q.InArr(forUser.Id, UserBuddies).Not()), 0, UserFields(UserId, UserNick, UserDtMade), UserDtMade.Desc())
-	buddyRequestsBy = yodb.FindMany[User](ctx, q.InArr(UserBuddies, forUser.Id), 0, nil)
+	buddiesAlready = yodb.FindMany[User](ctx,
+		UserId.In(forUser.Buddies.ToAnys()...).And(
+			q.InArr(forUser.Id, UserBuddies)),
+		0, nil, UserLastSeen.Desc(), UserDtMod.Desc())
+	buddyRequestsMade = yodb.FindMany[User](ctx,
+		UserId.In(forUser.Buddies.ToAnys()...).And(
+			q.InArr(forUser.Id, UserBuddies).Not()),
+		0, UserFields(UserId, UserNick, UserDtMade), UserDtMade.Desc())
+	buddyRequestsBy = yodb.FindMany[User](ctx,
+		q.InArr(UserBuddies, forUser.Id).And(
+			UserId.NotIn(forUser.Buddies.ToAnys()...)),
+		0, nil)
+
 	if normalizeLastSeenByMinute {
 		for _, buddy := range buddiesAlready {
 			if buddy.LastSeen != nil {
