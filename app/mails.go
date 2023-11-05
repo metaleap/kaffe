fb7803dcc03fcc162730fdd499c32aa081d78714
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-05 12:28:51 +0100

    sign-up/pwd-reset cont.

--- app/mails.go
+++ app/mails.go
@@ -9,13 +9,14 @@ import (
 )
 
 const mailTmplVarHref = "href"
+const mailTmplVarReqTime = "req_time"
 
 var mailTmpl = `
 Hi {` + yoauth.MailTmplVarName + `},
 
 you (or someone trolling you) requested that you {action} at {` + mailTmplVarHref + `}.
 
-If you did not initiate this request, simply delete this email.
+If you did not request this (at around {` + mailTmplVarReqTime + `} UTC), simply delete this email.
 
 Otherwise, go to {` + mailTmplVarHref + `} and enter the following 2 passwords:
 
@@ -23,24 +24,24 @@ Otherwise, go to {` + mailTmplVarHref + `} and enter the following 2 passwords:
 
 {` + yoauth.MailTmplVarTmpPwd + `}
 
-2. under "new password": your actual preferred password for future logins.
+2. under "new password": your own chosen password for future logins.
 
 Rock on!
 `
 
 func init() {
 	yomail.Templates[yoauth.MailTmplIdSignUp] = &yomail.Templ{
-		Vars:    []string{mailTmplVarHref, yoauth.MailTmplVarTmpPwd, yoauth.MailTmplVarName},
+		Vars:    []string{mailTmplVarHref, mailTmplVarReqTime, yoauth.MailTmplVarTmpPwd, yoauth.MailTmplVarName},
 		Subject: "Your sign-up request at " + appDomain,
 		Body:    str.Repl(mailTmpl, str.Dict{"action": "sign up"}),
 	}
 	yomail.Templates[yoauth.MailTmplIdPwdForgot] = &yomail.Templ{
-		Vars:    []string{mailTmplVarHref, yoauth.MailTmplVarTmpPwd, yoauth.MailTmplVarName},
+		Vars:    []string{mailTmplVarHref, mailTmplVarReqTime, yoauth.MailTmplVarTmpPwd, yoauth.MailTmplVarName},
 		Subject: "Your password-reset request at " + appDomain,
 		Body:    str.Repl(mailTmpl, str.Dict{"action": "reset your password"}),
 	}
 
-	yoauth.AppSideTmplPopulate = func(ctx *yojobs.Context, emailAddr yodb.Text, existingMaybe *yoauth.UserAuth, tmplArgsToPopulate yodb.JsonMap[string]) {
+	yoauth.AppSideTmplPopulate = func(ctx *yojobs.Context, reqTime *yodb.DateTime, emailAddr yodb.Text, existingMaybe *yoauth.UserAuth, tmplArgsToPopulate yodb.JsonMap[string]) {
 		var user *User
 		if existingMaybe != nil {
 			user = yodb.FindOne[User](ctx.Ctx, UserAuth.Equal(existingMaybe.Id))
@@ -49,5 +50,6 @@ func init() {
 			tmplArgsToPopulate[yoauth.MailTmplVarName] = string(user.Nick)
 		}
 		tmplArgsToPopulate[mailTmplVarHref] = "https://sesh.cafe"
+		tmplArgsToPopulate[mailTmplVarReqTime] = reqTime.Time().Format("15:04:05")
 	}
 }
